d912944b70f341e52955c71567e641acac6e4491
Laxmi Kandel <laxmikandel97@gmail.com>
2020-01-11 00:56:10 -0500

    made seperate file for assoc array and input name sticky and displayed approprate errors

--- index.php
+++ index.php
@@ -13,13 +13,14 @@
     <title>Cupcake!</title>
 </head>
 <body>
-<form id="CupcakeForm" action="confirmation.php" method="post">
-
+<form id="CupcakeForm" action="#" method="post" onsubmit="return false">
     <div class="container">
         <h1>Cupcakes Fundraiser</h1>
+        <img class="img-thumbnail rounded float-right" width="200" height="300" src="cupcake.jpg" alt="cupcake image">
+
         Your name:<br>
         <div class="row">
-            <input class="col-6" type="text" id="name" name="name" placeholder="Please input your name!"><br>
+            <input class="col-6" type="text" id="name" name="name" value="<?php if(isset($_POST['name'])){ echo $_POST['name'];} ?>" placeholder="Please input your name!"><br>
             <span class="err" id="err-name">
                         Please enter your  name</span>
         </div>
@@ -27,60 +28,45 @@
 
         <div class="form-group">
             <?php
-            $flavors = array(
-                "grasshopper" => "The Grasshopper",
-                "maple" => "Whiskey Maple Bacon",
-                "carrot" => "Carrot Walnut",
-                "caramel" => "Salted Caramel Cupcake",
-                "velvet" => "Red Velvet",
-                "lemon" => "Lemon Drop",
-                "tiramisu" => "Tiramisu",
-            );
-            foreach ($flavors as $key => $value)
-            {
-                echo " <input class='form-check-inline' type='checkbox' name='flavor[]' value='$key'->$value</input>" . "<br>";
+            include "functions.php";
+            foreach ($flavors as $key => $value) {
+                echo " <input class='form-check-inline' type='checkbox' name='flavor[]' value='$key'=>$value  </input>" . "<br>";
             }
-//            $getFlavor=$_POST["flavor"];
-//            echo "<ul>";
-//            foreach ($getFlavors as $item)
-//            {
-//                echo "<li>".$flavors['$item'] ."</li>";
-//
-//            }
-//            echo "</ul>";
-
-            //            if(isset($getFlavor)){
-//                foreach ($getFlavor as $item){
-//                    foreach ($item as $key=>$pair)
-//                    {
-//                        echo $pair." ";
-////                        if (array_key_exists($item, $flavors)) {
-////                            echo "$pair ";
-//////                        break;
-////                        }
-////                        else{
-////                            echo "<p class='text-danger'> Invalid value for flavor</p> ";
-////                        }
-//                    }
-////
-//                }
-//            }
             ?>
             <div>
                 <span class="err" id="err-flavor">Please select at least one interest</span>
             </div>
         </div>
-        <!--    <input type="checkbox" name="flavor" value="The Grasshopper"> The Grasshopper<br>-->
-        <!--    <input type="checkbox" name="flavor" value="Whiskey Maple Bacon"> Whiskey Maple Bacon<br>-->
-        <!--    <input type="checkbox" name="flavor" value="Carrot Walnut"> Carrot Walnut<br>-->
-        <!--    <input type="checkbox" name="flavor" value="Salted Caramel Cupcake"> Salted Caramel Cupcake<br>-->
-        <!--    <input type="checkbox" name="flavor" value="Red Velvet"> Red Velvet<br>-->
-        <!--    <input type="checkbox" name="flavor" value="Lemon Drop"> Lemon Drop<br>-->
-        <!--    <input type="checkbox" name="flavor" value="Tiramisu"> Tiramisu <br>-->
-        <button type="submit" class="btn btn-success">Order</button>
+        <button type="submit" name="submit" class="btn btn-success mb-3">Order</button>
+    </div>
+    <div class="container">
+        <?php
+        if(isset($_POST['submit'])) {
+            $name = $_POST["name"];
+            $getFlavors = $_POST["flavor"];
+            $count = 0;
+            echo " Thank you, " . $name . ", Below is your order";
+            echo " Order Summary:" . "<br>";
+            echo "<ul>";
+            $isvalid = true;
+            foreach ($getFlavors as $item) {
+                if (array_key_exists("$item", $flavors)) {
+                    echo "<li>" . $flavors["$item"] . "</li>";
+                    $count++;
+                } else {
+                    echo "<p class='text-danger'> Your Order is not been place. Please check your options!</p>";
+                    $isvalid = false;
+                    break;
+                }
+            }
+            if (($isvalid)) {
+                echo "<h5 class='text-info'> Total Order:  $" . $count * 3.50 . "</h5>";
+                echo "</ul>";
+            }
+        }
+        ?>
     </div>
 </form>
-
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
@@ -93,5 +79,6 @@
         integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
         crossorigin="anonymous"></script>
 <script src="cupcake.js"></script>
+
 </body>
 </html>
